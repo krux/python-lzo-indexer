503e21f7a1870641d7c494ef53f0ad894d00e491
Paul Lathrop <paul@tertiusfamily.net>
2014-04-03 17:00:44 -0700

    Fix long description to make it installable.

--- setup.py
+++ setup.py
@@ -9,7 +9,19 @@ def read(filename):
 setup(name="lzo-indexer",
     version="0.0.1",
     description="Library for indexing LZO compressed files",
-    long_description=read("README.md"),
+    long_description="""
+
+    Python library for indexing block offsets within LZO compressed
+    files. The implementation is largely based on that of the [Hadoop
+    Library](https://github.com/twitter/hadoop-lzo). Index files are used
+    to allow Hadoop to split a single file compressed with LZO into several
+    chunks for parallel processing.
+
+    Since LZO is a block based compression algorithm, we can split the file
+    along the lines of blocks and decompress each block on it's own. The
+    index is a file containing byte offsets for each block in the original
+    LZO file.
+    """,
     author="Tom Arnfeld",
     author_email="tom@duedil.com",
     maintainer="Tom Arnfeld",
